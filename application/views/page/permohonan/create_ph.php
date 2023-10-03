8f07252179e84a9a4c68c9564d3078fa2d1d5809
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-03 13:54:14 +0700

    update

--- application/views/page/permohonan/create_ph.php
+++ application/views/page/permohonan/create_ph.php
@@ -60,11 +60,7 @@
                     <?=get_form_input($model, 'subject'); ?>
                     <?=get_form_input($model, 'kasus_posisi', array('type' => 'textarea', 'rows' => '3', 'cols' => '10')); ?>
 
-                    <?=get_form_input($model, 'dokumen[]', array('type' => 'file')); ?>
-                    <?=get_form_input($model, 'dokumen[]', array('type' => 'file')); ?>
-                    <?=get_form_input($model, 'dokumen[]', array('type' => 'file')); ?>
-                    <?=get_form_input($model, 'dokumen[]', array('type' => 'file')); ?>
-                    <?=form_hidden('status', '1'); ?>
+                    <?=get_form_input($model, 'dokumen[]', array('type' => 'file', 'multiple' => 'multiple')); ?>
 
                     <button type="submit" class="btn btn-primary" id="form-submit">Submit Permohonan</button>
                     <button type="reset" class="btn btn-default">Reset</button>
@@ -88,13 +84,15 @@ $( document ).ready(function() {
         e.preventDefault();
 
         var fd = new FormData();
-        // var files = $('#input-dokumen[]')[0].files[0];
-        var totalfiles = document.getElementById('input-dokumen[]').files.length;
-        for (var index = 0; index < totalfiles; index++) {            
-            var files = $('#input-dokumen[]')[0].files[index];
-            fd.append('file',files);
+        var files = $('#input-dokumen[]')[0].files[0];
+        fd.append('file',files);
+
+        // var totalfiles = $('#input-dokumen[]')[0].files.length;
+        // for (var index = 0; index < totalfiles; index++) {            
+            // var files = $('#input-dokumen[]')[0].files[index];
+            // fd.append('file',files);
             // fd.append("input-dokumen[]", document.getElementById('input-dokumen[]').files[index]);
-        }
+        // }
 
         if(files?.length){
             alert("Please select a file.");
