15bcc8f9de72860be741bac1606acdd51f0a36f9
Agus Puryanto <aguspuryanto@gmail.com>
2023-10-04 17:49:34 +0700

    update download

--- application/views/page/permohonan/konsiliasi.php
+++ application/views/page/permohonan/konsiliasi.php
@@ -49,7 +49,7 @@
                                 $isArray = json_decode($row->dokumen, true); //explode(",", $row->dokumen);                                
                                 if(is_array($isArray)) {
                                     foreach($isArray as $key => $dok) {
-                                        $dokUrl .= '<a target="_blank" href="'.base_url('permohonan/dokumen/' . $dok).'" class="btn btn-link btn-block">Dokumen ' . $key . '</a>';
+                                        $dokUrl .= '<a target="_blank" href="'.base_url('permohonan/dokumen/' . $dok).'" class="btn btn-link btn-block">Dokumen ' . ($key+1) . '</a>';
                                     }
                                 } else {
                                     $dokUrl = ($row->dokumen) ? '<a target="_blank" href="'.base_url('permohonan/dokumen/' . $row->dokumen).'" class="btn btn-link btn-block">Dokumen</a>' : '#';
